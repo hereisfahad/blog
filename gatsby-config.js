ea1d7fc4103e0af89ca15dc523b845d03f205209
hereisfahad <fahadjabbar31@gmail.com>
2019-10-17 20:02:09 +0500

    initial commit..adding files

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Today I learn`,
+    author: `hereisfahad`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/hereisfahad`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/hereisfahad`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    {/* <br/> */}
+    This is <Styled.a href="http://hereisfahad.netlify.com/">Fahad Jabbar</Styled.a>
+    {` `}
+    <br />
+    He writes here.
+  </Fragment>
+)
